f83b9ec4dc8be37197fde6a97984ea41ae49c50a
Daniel Molina <danielmolina9202@gmail.com>
2024-08-10 19:38:49 -0400

    terminado el componente Header

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Navbar from './Components/Navbar'
+import Header from './Components/Header'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <Header />
       <Navbar />
     </>
   )
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function Navbar() {
   return (
-    <h1>Prueba</h1>
+    <h1>Pruebaaa</h1>
   )
 }
