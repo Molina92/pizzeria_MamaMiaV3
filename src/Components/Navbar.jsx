9c1dc72a2e691608101dd4f5ea93256bbd5d07d5
Daniel Molina <danielmolina9202@gmail.com>
2024-08-10 20:14:03 -0400

    Componente Navbar terminado

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,7 +1,43 @@
-import React from 'react'
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import Button from 'react-bootstrap/Button';
 
-export default function Navbar() {
+export default function CustomNavbar() {
+
+  const total = 25000;
+  const token = false;
+
+  const formatearMontoEnCLP = (monto) => {
+    return monto.toLocaleString("es-CL", { style: "currency", currency: "CLP" });
+  };
+
   return (
-    <h1>Pruebaaa</h1>
+    <Navbar bg="dark" variant="dark" expand="lg">
+      <Container>
+        <Navbar.Brand href="#home">Pizzería Mamma Mia!</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">🍕 Home</Nav.Link>
+            {token ? (
+              <>
+                <Nav.Link href="#profile">🔓 Profile</Nav.Link>
+                <Nav.Link href="#logout">🔒 Logout</Nav.Link>
+              </>
+            ) : (
+              <>
+                <Nav.Link href="#login">🔐 Login</Nav.Link>
+                <Nav.Link href="#register">🔐 Register</Nav.Link>
+              </>
+            )}
+          </Nav>
+          <Button variant="primary" >
+            🛒 Total: {formatearMontoEnCLP(total)}
+          </Button>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   )
 }
